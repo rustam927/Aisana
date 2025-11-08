69310aa14b715bfa85553fa649638446832f4c22
rustam927 <rustamrahmonov927@gmail.com>
2025-11-08 16:43:05 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function divs() {
+  let divf = document.querySelector(".divf")
+  let divf1 = document.querySelector(".divf1")
+
+  let divf2 = document.querySelector(".divf2")
+  let divf3 = document.querySelector(".divf3")
+
+  let divf4 = document.querySelector(".divf4")
+  let divf5 = document.querySelector(".divf5")
+
+
+  divf.style.top = "300px"
+}
+
+setTimeout(divs, 5000);
